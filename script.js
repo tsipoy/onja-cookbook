61c0819ff460c69b80e1d5c4162ba991c89dd349
Natacha <natacha.har@onja.org>
2020-07-25 14:17:14 +0300

    Added event listener to listen the the add buttons

---
+++ script.js
@@ -0,0 +1,45 @@
+// Grab elements
+const divWrapper = document.querySelector('.cook-book');
+const form = document.querySelector('form');
+const name = document.querySelector('.recipe-name');
+const nameInput = document.querySelector('#name');
+const photoUrl = document.querySelector('.food-picture');
+const url = document.querySelector('.foo-picture');
+const UrlInput = document.querySelector('#url');
+const chef = document.querySelector('.recipe-chef');
+const chefInput = document.querySelector('#chef');
+const steps = document.querySelector('.new-steps');
+const ingredients = document.querySelector('.new-ingredient');
+const ingredientsInput = document.querySelector('#ingredients-input');
+const stepsInput = document.querySelector('#steps-input');
+const ingredientsBtn = document.querySelector('.ingredients-button');
+const stepsBtn = document.querySelector('.steps-button');
+
+
+const ingredientsList = () => {
+    const myIngredients = `
+    <div class="ingredients">
+        <div class="new-ingredients">
+            <span class="set-ingredients">${ingredientsInput.value}</span>
+        </div>
+    </div>
+    `;
+    ingredients .insertAdjacentHTML('afterbegin', myIngredients);
+};
+
+const stepsList = () => {
+    const mySteps = `
+    <div class="steps">
+        <div class="new-steps">
+            <span class="set-steps">${stepsInput.value}</span>
+        </div>
+    </div>
+    `;
+    steps.insertAdjacentHTML('afterbegin', mySteps);
+};
+
+ingredientsBtn.addEventListener('click', ingredientsList);
+stepsBtn.addEventListener('click', stepsList);
+
+
+
